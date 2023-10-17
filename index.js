1298797e17c71b70a609f6720a3ec94797c57913
btcoyne <bcoyne3@gmail.com>
2023-10-17 09:58:21 -0700

    add some pyodide boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import './style.css';
-import {Geometry} from './dfGeo.js';
+//import {Geometry} from './dfGeo.js';
 
 // Write Javascript code!
 //const dfd = require("danfojs-node")
@@ -12,11 +12,11 @@ let json_data = [{ x: 0.4612, y: 4.28283, z: -1.509 },
   { x: 0.6911, y: -0.82863, z: -1.5059 },
   { x: 0.4692, y: -1.28863, z: 4.5059 }];
 
-let geo = new Geometry(json_data);
-geo.print("points");
-geo.rotate(Math.PI,0,0);
-geo.print("points");
-geo.scale(10,-1,1);
-geo.print("points");
+// let geo = new Geometry(json_data);
+// geo.print("points");
+// geo.rotate(Math.PI,0,0);
+// geo.print("points");
+// geo.scale(10,-1,1);
+// geo.print("points");
 
 
